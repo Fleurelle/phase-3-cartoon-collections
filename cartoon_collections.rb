8367feaeab6c9b8a5d8e003288e07accf6c4cac0
Rose Jean Louis <r.d.jeanlouis01@gmail.com>
2022-02-12 10:28:29 -0500

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,17 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(queue)
+  counter = 1
+  queue.map do |dwarf|
+    puts "#{counter}. #{queue}"
+    counter += 1
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(elements)
+  newArray = []
+  elements.map do |element|
+    newArray << element.capitalize
+
+  end
 end
 
 def long_planeteer_calls# code an argument here
